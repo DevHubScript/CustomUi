bbeb0e6ede660fd3e304ef079a6d56f4a8ac6e85
StaterCoding <98807399+DevHubScript@users.noreply.github.com>
2024-03-23 22:20:49 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1293,22 +1293,22 @@ function library:AddWindow(text,keybind)
     return Ui
 end
 
-if string.lower(game:GetService("RbxAnalyticsService"):GetClientId()) == game:GetService("RbxAnalyticsService"):GetClientId() then
-    local ScreenGui = Instance.new("ScreenGui")
-    local ImageButton = Instance.new("ImageButton")
+--if string.lower(game:GetService("RbxAnalyticsService"):GetClientId()) == game:GetService("RbxAnalyticsService"):GetClientId() then
+    --local ScreenGui = Instance.new("ScreenGui")
+    --local ImageButton = Instance.new("ImageButton")
 
-    ScreenGui.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+    --ScreenGui.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
 
-    ImageButton.Parent = ScreenGui
-    ImageButton.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
-    ImageButton.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
-    ImageButton.Size = UDim2.new(0, 50, 0, 50)
-    ImageButton.Image = "rbxassetid://8622542227"
-    ImageButton.MouseButton1Down:connect(function()
-        game:GetService("VirtualInputManager"):SendKeyEvent(true,305,false,game)
-        game:GetService("VirtualInputManager"):SendKeyEvent(false,305,false,game)
-    end)
-end
+    --ImageButton.Parent = ScreenGui
+    --ImageButton.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
+    --ImageButton.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
+    --ImageButton.Size = UDim2.new(0, 50, 0, 50)
+    --ImageButton.Image = "rbxassetid://8622542227"
+    --ImageButton.MouseButton1Down:connect(function()
+        --game:GetService("VirtualInputManager"):SendKeyEvent(true,305,false,game)
+        --game:GetService("VirtualInputManager"):SendKeyEvent(false,305,false,game)
+    --end)
+--end
 
 local win = library:AddWindow("")
 
